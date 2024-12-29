267ee9cb10a3adec64f7a0d322c9f6b13902d40b
Zeel <pzeel881@gmail.com>
2024-12-28 22:44:22 -0500

    Working

--- src/components/UI/price.jsx
+++ src/components/UI/price.jsx
@@ -1,20 +1,23 @@
 import React from "react"
+import Book from "./books"
 
-const Price = (salePrice, originalPrice) => {
+const Price = ({ salePrice, originalPrice }) => {
+
      return (
-          <div className="book__price">
-               {
-                    book.salePrice
-                         ? (
-                              <><span className="book__price--normal">
-                                   ${originalPrice.toFixed(2)}
-                                   </span>
-                                   ${salePrice.toFixed(2)}</>
-                         ) : (
-                              <>${originalPrice.toFixed(2)}</>
-                         )}
-          </div>
-     )
-}
 
-export default Price
+          <div className="book__price">
+               {Book.salePrice ? (
+                    <>
+                         <span className="book__price--normal">
+                              ${originalPrice.toFixed(2)}
+                         </span>
+                         ${salePrice.toFixed(2)}
+                    </>
+               ) : (
+                    <>${originalPrice.toFixed(2)}</>
+               )}
+          </div>
+     );
+};
+
+export default Price;
